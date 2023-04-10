7bc103d5336aca9491395d47515bfe903e75525f
Yoonjit <jisu9083@naver.com>
2023-04-10 20:43:39 +0900

    프롬프트 + 조건문

--- Web/0410-2/main.js
+++ Web/0410-2/main.js
@@ -6,3 +6,21 @@ name2 = "윤지지지수"
 
 console.log(name1)
 console.log(name2)
+
+const x = 3
+const y = 2
+
+console.log(x + y) // 5
+console.log(x - y) // 1
+console.log(x * y) // 6
+console.log(x / y) // 1.5
+console.log(x % y) // 1
+
+let q = prompt("무엇을 주문하시겠습니까?", "짜장면 or 짬뽕") // '빨리 입력해'는 없어도 무방
+if (q === "짜장면") {
+    console.log("7,000원입니다.")
+} else if (q === "짬뽕") {
+    console.log("7,500원입니다.")
+} else {
+    console.log("그런 거 안 팔아요.")
+}
